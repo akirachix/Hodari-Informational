7aa5eb4a9424a7eecb2c82735d0f686ac0e1d933
AmandaIreri <amandairerii@gmail.com>
2024-06-18 14:35:57 +0300

    added footer

--- inota-plus/src/App.js
+++ inota-plus/src/App.js
@@ -1,18 +1,16 @@
 import Nav from "./Navbar";
 import About from "./About";
 import Contact from "./Contact"
+import Footer from "./Footer/footer";
 
 const App = () =>{
   return(
-   
-      <div>
-         <Nav/>
-         <About/>
-         <Contact/>
-
-        
-
-      </div>  
-  )
+    <div>
+      <Nav />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
 export default App;
